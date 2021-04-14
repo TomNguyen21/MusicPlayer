36233a448dca19ca366051cfbcde7681339d795a
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-04-13 21:52:26 -0700

    Add reducer boilerplate code

---
+++ spotify-clone/src/reducer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import reducer, { initialState } from "./reducer";
+import { DataLayer } from "./DataLayer";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
