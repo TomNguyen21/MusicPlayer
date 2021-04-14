7074a57b3708a0ffb1c811704b41681343b10096
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-04-13 21:33:27 -0700

    Implement basic login page, no logic

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import './App.css';
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
-      React App
+      <Login />
     </div>
   );
 }
