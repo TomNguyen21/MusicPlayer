12c188ccc23ac2452ca9b23bd8fb159e5ad32884
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-04-13 21:46:10 -0700

    Allow app to tell if user is logged in or not

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+
+    console.log("token", token);
+  }, []);
+
   return (
     <div className="App">
-      <Login />
+      {token ? <h1>Logged In</h1> : <Login />}
     </div>
   );
 }
