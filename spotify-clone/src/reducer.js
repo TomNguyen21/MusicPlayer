cfebdebf51bab676dd2c5d8bd10668c0964acac3
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-04-15 20:30:15 -0700

    Add player js and css files

--- spotify-clone/src/reducer.js
+++ spotify-clone/src/reducer.js
@@ -1,16 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import reducer, { initialState } from "./reducer";
-import { DataLayer } from "./DataLayer";
+export const initialState = {
+  user: null,
+  token: null,
+  playlists: [],
+  playing: false,
+  item: null,
+};
 
-ReactDOM.render(
-  <React.StrictMode>
-    <DataLayer initialState={initialState} reducer={reducer}>
-      <App />
-    </DataLayer>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const reducer = (state, action) => {
+  console.log(action);
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
