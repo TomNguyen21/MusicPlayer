309534decb2dfaa9e4cd577b274d08bf57f17fe2
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-05-02 15:04:41 -0700

    Add player for when logged in

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
 
 const spotify = new SpotifyWebApi();
 
@@ -24,7 +25,7 @@ function App() {
 
   return (
     <div className="App">
-      {token ? <h1>Logged In</h1> : <Login />}
+      {token ? <Player /> : <Login />}
     </div>
   );
 }
--- spotify-clone/src/reducer.js
+++ spotify-clone/src/reducer.js
@@ -1,38 +1,16 @@
-export const initialState = {
-  user: null,
-  token: null,
-  playlists: [],
-  playing: false,
-  item: null,
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import reducer, { initialState } from "./reducer";
+import { DataLayer } from "./DataLayer";
 
-const reducer = (state, action) => {
-  console.log(action);
-
-  switch (action.type) {
-    case "SET_USER":
-      return {
-        ...state,
-        user: action.user,
-      };
-    case "SET_TOKEN":
-      return {
-        ...state,
-        token: action.token,
-      };
-    case "SET_PLAYLISTS":
-      return {
-        ...state,
-        playlists: action.playlists,
-      };
-    case "SET_DISCOVER_WEEKLY":
-      return {
-        ...state,
-        discover_weekly: action.discover_weekly,
-      };
-    default:
-      return state;
-  }
-};
-
-export default reducer;
+ReactDOM.render(
+  <React.StrictMode>
+    <DataLayer initialState={initialState} reducer={reducer}>
+      <App />
+    </DataLayer>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
