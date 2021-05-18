d27d283e2cf3b6b2df5b0a907421862756ffe2ad
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-05-17 19:36:18 -0700

    Add default token status to reducer

--- spotify-clone/src/reducer.js
+++ spotify-clone/src/reducer.js
@@ -3,6 +3,7 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
+  token: null,
 }
 
 const reducer = (state, action) => {
@@ -16,6 +17,11 @@ const reducer = (state, action) => {
         ...state,
         user: action.user
       };
+    case 'SET_TOKEN':
+      return {
+        ...state,
+        token: action.token
+      }
     default:
       return state;
   }
