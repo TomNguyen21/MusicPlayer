055a0a89dfcd43134eadbc6ef5c5d603ced79b0a
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-05-17 23:17:34 -0700

    Config to pull playlist from Spotify API

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -29,6 +29,13 @@ function App() {
         dispatch({
           type: 'SET_USER',
           user: user,
+        });
+      });
+
+      spotify.getUserPlaylists().then( (playlists) => {
+        dispatch({
+          type: 'SET_PLAYLISTS',
+          playlists: playlists,
         })
       })
     }
--- spotify-clone/src/reducer.js
+++ spotify-clone/src/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
   playing: false,
   item: null,
   // REMOVE AFTER FINISHED DEVELOPING ----- change back to null
-  token: "BQCPm9cm_2_EM356E36KJ1qEmY056GrL67kgTcmWlZBR2_mDj3mrvpfl3fiLBVcao0nLLKlTwC4YCL7hp8S7U__UZWusF4jIbDH8LCIjHR54kLp2MY2ZUgTqvzkMUuOADAcq1UfqQOnx7FTjllIEfIMCIiAgXr7qYA",
+  // token: "BQCPm9cm_2_EM356E36KJ1qEmY056GrL67kgTcmWlZBR2_mDj3mrvpfl3fiLBVcao0nLLKlTwC4YCL7hp8S7U__UZWusF4jIbDH8LCIjHR54kLp2MY2ZUgTqvzkMUuOADAcq1UfqQOnx7FTjllIEfIMCIiAgXr7qYA",
 }
 
 const reducer = (state, action) => {
@@ -22,7 +22,12 @@ const reducer = (state, action) => {
       return {
         ...state,
         token: action.token
-      }
+      };
+    case 'SET_PLAYLISTS':
+      return {
+        ...state,
+        playlists: action.playlists
+      };
     default:
       return state;
   }
