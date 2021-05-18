7c9910412628723556033b903c8973d58d362bb2
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-05-17 21:56:42 -0700

    Add more CSS to Body and Sidebar

--- spotify-clone/src/Sidebar.js
+++ spotify-clone/src/Sidebar.js
@@ -4,6 +4,7 @@ import './Sidebar.css';
 function Sidebar() {
   return (
     <div className="sidebar">
+      <img className="sidebar_logo" src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt="spotify logo" />
       <h1>I am the Sidebar</h1>
     </div>
   )
