ff5d48584035187ea7a1aabac8adb2220e7341cd
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-05-17 21:19:07 -0700

    Add some basic css for sidebar and body

--- spotify-clone/src/Player.js
+++ spotify-clone/src/Player.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './Player.css';
 import Sidebar from './Sidebar';
+import Body from './Body';
 
 function Player({ spotify }) {
   return (
     <div className="Player">
       <div className="player_body">
         <Sidebar />
-        {/* Sidebar */}
-        {/* Body */}
+        <Body />
 
         {/* Footer */}
       </div>
