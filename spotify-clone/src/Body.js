dc5b52d1abef28b12d342bc2f9f3d0a656f332dd
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-05-19 22:21:28 -0700

    Create banner for the body

--- spotify-clone/src/Body.js
+++ spotify-clone/src/Body.js
@@ -6,6 +6,16 @@ function Body({ spotify }) {
   return (
     <div className="body">
         <Header spotify={spotify} />
+
+        <div className="body_info">
+          <img src="https://www.hypebot.com/wp-content/uploads/2020/07/discover-weekly.png" alt="image of playlists" />
+          <div className="body_infoText">
+            <strong>PLAYLIST</strong>
+            <h2>Discover Weekly</h2>
+            <p>description...</p>
+          </div>
+        </div>
+
     </div>
   )
 }
--- spotify-clone/src/reducer.js
+++ spotify-clone/src/reducer.js
@@ -4,7 +4,7 @@ export const initialState = {
   playing: false,
   item: null,
   // REMOVE AFTER FINISHED DEVELOPING ----- change back to null
-  // token: "BQCPm9cm_2_EM356E36KJ1qEmY056GrL67kgTcmWlZBR2_mDj3mrvpfl3fiLBVcao0nLLKlTwC4YCL7hp8S7U__UZWusF4jIbDH8LCIjHR54kLp2MY2ZUgTqvzkMUuOADAcq1UfqQOnx7FTjllIEfIMCIiAgXr7qYA",
+  token: "BQCPm9cm_2_EM356E36KJ1qEmY056GrL67kgTcmWlZBR2_mDj3mrvpfl3fiLBVcao0nLLKlTwC4YCL7hp8S7U__UZWusF4jIbDH8LCIjHR54kLp2MY2ZUgTqvzkMUuOADAcq1UfqQOnx7FTjllIEfIMCIiAgXr7qYA",
 }
 
 const reducer = (state, action) => {
