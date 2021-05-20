8af78d376b8c5c0dc9d1e38f9121057a38ba4f36
Tom Nguyen <tommy.nguyen0721@gmail.com>
2021-05-19 22:13:34 -0700

    Create header for for page

--- spotify-clone/src/Body.js
+++ spotify-clone/src/Body.js
@@ -5,7 +5,6 @@ import Header from './Header';
 function Body({ spotify }) {
   return (
     <div className="body">
-      <h1>I am the body</h1>
         <Header spotify={spotify} />
     </div>
   )
--- spotify-clone/src/Header.js
+++ spotify-clone/src/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import { Avatar } from '@material-ui/core';
+import { useDataLayerValue } from './DataLayer';
 
 function Header() {
+  const [{ user }, dispatch] = useDataLayerValue();
   return (
     <div className="header">
       <div className="header_left">
@@ -10,7 +13,8 @@ function Header() {
         <input placeholder="Search for Artists, Songs, or Albums" type="text" />
       </div>
       <div className="header_right">
-
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
       </div>
     </div>
   )
